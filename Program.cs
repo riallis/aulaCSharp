a0f80cc383234a2abc7f745715581e9f1d97d129
Riallis <riallis.franca@gmail.com>
2018-04-20 11:13:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia");
+
+            Console.ReadLine();
+        }
+    }
+}
