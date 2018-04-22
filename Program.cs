8d29138c5be32ded29f6563e62189afdbbe64e4f
Riallis <riallis.franca@gmail.com>
2018-04-22 16:25:53 -0300

    colorindo saida do terminal

--- Program.cs
+++ Program.cs
@@ -6,10 +6,18 @@ namespace projeto1
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            
             Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Bom Dia");
+            
+            Console.ResetColor();
 
             Console.ReadLine();
+            
+                      
         }
     }
 }
